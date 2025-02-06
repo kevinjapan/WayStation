6376698f3a4da02fbc209ff19035afe44c05ef89
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-02-06 11:46:34 +0000

    remove depr comments

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,24 +7,6 @@ class Task < ApplicationRecord
 
    has_many :comments, as: :commentable
 
-   # to do : add commentable to all artifacts
-   # see Active Record Associations : 4.1 Polymorphic Associations
-   # - search for example cli command to do this (above ref only shows manual)
-   # - first(?), create Comment class model..
-   # - then, make artifacts 
-   # eg
-   # class Comment < ApplicationRecord
-   #    belongs_to :commentable, polymorphic: true
-   #  end
-    
-   #  class Project < ApplicationRecord
-   #    has_many :comments, as: :commentable
-   #  end
-    
-   #  class Todo < ApplicationRecord
-   #    has_many :comments, as: :commentable
-   #  end
-
    has_rich_text :desc
    
    # ensure all Tasks have a title
